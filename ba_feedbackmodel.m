ea9aee9880e3cbae21f74e5a9315dcf83eda5467
John <jabatzoglou@ucmerced.edu>
2021-09-05 16:30:15 -0700

    Update ba_feedbackmodel.m

--- ba_feedbackmodel.m
+++ ba_feedbackmodel.m
@@ -1,14 +1,18 @@
-function [r,p,BA,ra,permrat,ratold,permratold]=ba_feedbackmodel(BA,Z,totalarea,futureZ,option,capannual,nmem,firehistory,firehistory1950);
+function [r,p,BA,ra,permrat,ratold,permratold]=ba_feedbackmodel(BA,Z,totalarea,futureZ,option,nmem,alphaval,nyeardrought,nensemble,mcap,firehistory,firehistory1950);
 
 % experimental function that builds in a negative feedback to burned area
-nensemble=1000;
-Z(6:end+5)=Z;
-Z5=movmean(Z,[5 0]);
-Z5=Z5(6:end);
-Z=Z(6:end);
+Z5=movmean(Z,[nyeardrought 0]);
+Z5=Z5(nyeardrought+1:end);
+Z=Z(nyeardrought+1:end);
+
+% only use up to nyeardrought-years of data for Z and Z5 -- this is determined by
+% nyeardrought parameter
+Z=Z(6-nyeardrought:end);
+Z5=Z5(6-nyeardrought:end);
 
 
-if nargin==7 % if you do not have fire history layer
+
+if nargin==10 % if you do not have fire history layer
 % SPINUP: assume some fraction of previous say 30-yrs have burnhistory
 % default it 0.1% of lands burn
 firehistory=0.001*ones(nmem,1);
@@ -22,7 +26,7 @@ end
 % reduce available area to burn as a function of fire history
 addrat=0;
 for i=1:length(BA)
- [rat,addrat,firehistory]=firehistorybufferadv(totalarea,firehistory,option,nmem,Z(i),Z5(i),addrat,0.1);
+ [rat,addrat,firehistory]=firehistorybufferadv(totalarea,firehistory,option,nmem,Z(i),Z5(i),addrat,alphaval,nyeardrought);
  firehistory(end+1)=BA(i)/totalarea;
  firehistory=firehistory(2:end);
  rrr(i)=rat+addrat;
@@ -42,6 +46,7 @@ mm=predict(a2,Z2);mm=(10.^mm)./(1-rat)';
 
 % residuals and constrained residuals
 residual_log=(log10(BA)-log10(mm));
+% constrain residuals to the middle 70 percentile if needed
 r1=prctile(residual_log,[15 85]);
 residual_log(residual_log<r1(1))=r1(1);
 residual_log(residual_log>r1(2))=r1(2);
@@ -50,9 +55,9 @@ residual_log(residual_log>r1(2))=r1(2);
 permratold=permoff;
 ratold=rat;
 
-
+% set reset1950 to 1 if you don't want to include observed fire history into dynamic models
 reset1950=0;
-if reset1950
+if reset1950 & nargin==12
 % if you want to reset burned area starting at 1950
 firehistory=firehistory1950;
 clear BA rat
@@ -64,19 +69,19 @@ end
 if option==0 startyr=1;end  % this is for no-feedback model
 
 % starting in 1950 iterative forward
-    Z5(6:156)=movmean(futureZ(1:151),[5 0]);
+    Z5(nyeardrought+1:nyeardrought+150)=movmean(futureZ(1:150),[nyeardrought 0]);
     addrat=0;
     
     initfhist=firehistory;
     for kk=1:nensemble
         firehistory=initfhist;addrat=0;
 for i=startyr:151
-    [rat,addrat,firehistory]=firehistorybufferadv(totalarea,firehistory,option,nmem,futureZ(i),Z5(i),addrat,.1);
+    [rat,addrat,firehistory]=firehistorybufferadv(totalarea,firehistory,option,nmem,futureZ(i),Z5(i),addrat,alphaval,nyeardrought);
      BA(i,kk)=10.^(predict(a2,(futureZ(i)))+residual_log(ceil(rand(1,1)*37))).*(1-rat);
     
     % limit such that the burned area annually can not
     % exceed more than X% of existing land
-    BA(i,kk)=min(BA(i,kk),(1-rat)*totalarea*capannual);
+    BA(i,kk)=min(BA(i,kk),(1-rat)*totalarea*mcap);
     ra(i,kk)=rat;
     permrat(i,kk)=addrat;
     firehistory(end+1)=BA(i,kk)/totalarea;
