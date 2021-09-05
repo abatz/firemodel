9b9ceed6ab4873fa1372d3d0c5562c74cbf983ba
John <jabatzoglou@ucmerced.edu>
2021-09-05 16:22:55 -0700

    Update firehistorybufferadv.m

--- firehistorybufferadv.m
+++ firehistorybufferadv.m
@@ -1,21 +1,21 @@
-function [rat,permrat,firehistory]=firehistorybuffer(totalarea,firehistory,option,nmem,Z,Z5,permrat,max_norecovery);
+function [rat,permrat,firehistory]=firehistorybuffer(totalarea,firehistory,option,nmem,Z,Z5,permrat,max_norecovery,nyeardrought);
 
 
-% Z5 is the mean aridity from the past 5-years, this is used to immediately
+% Z5 is the mean aridity from the past nyeardrought-years, this is used to immediately
 % pull a percent of burned area out of rotation (forest --> non-forest)
 
-% max_norecovery: what is the maximum extent of burned area from 6-yrs ago that does not
+% max_norecovery: what is the maximum extent of burned area from (1+nyeardrought)-yrs ago that does not
 
-% scales linear function of aridity in past 5-yrs
+% scales linear function of aridity in past nyeardrought-yrs
 %percent_norecovery=(cos(max(min(1,Z5-1)-1,-1)*90*pi/180)+1)/1;
 percent_norecovery(Z5<1)=0;
 percent_norecovery(Z5>2)=1;
 percent_norecovery(Z5>=1 & Z5<=2)=Z5(Z5>=1 & Z5<=2)-1;
-removedforest=max_norecovery*percent_norecovery.*firehistory(end-5,:);
+removedforest=max_norecovery*percent_norecovery.*firehistory(end-nyeardrought,:);
 
-% burned area from 6 years ago is modified
+% burned area from (1+nyeardrought) years ago is modified
 addrat=removedforest;
-firehistory(end-5,:)=firehistory(end-5,:)-removedforest;
+firehistory(end-nyeardrought,:)=firehistory(end-nyeardrought,:)-removedforest;
 
 % build this back in at the end
 fh=firehistory;
@@ -28,7 +28,6 @@ permrat=permrat+addrat;
 shorten(Z>2)=round(nmem/3);
 shorten(Z>0 & Z<=2)=round(nmem/3*Z(Z>0 & Z<=2)/2);
 shorten(Z<=0)=0;
-%shorten=(cos(max(min(2,Z)-2,-2)*90*pi/180)+1)/2;
 firehistory=firehistory(1+shorten:end,:);
 nmem=nmem-shorten;
 
