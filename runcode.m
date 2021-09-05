3daf001ac04e994a022043734d5acbed3fb9c871
John <jabatzoglou@ucmerced.edu>
2021-09-05 16:41:56 -0700

    Update runcode.m

--- runcode.m
+++ runcode.m
@@ -7,18 +7,28 @@ BA=obsdata(:,2);
 yr=1984-1949:2020-1949;
 BA=BA(yr);
 Z=Z(yr);
-%area of forest extent from LANDFIRE ESP
+%area of forest/woodland extent from LANDFIRE ESP
 totalarea=833330;
 
 % reference fuel memory (years)
 nmem=30;
 
+% number of simulations
+nensemble=1000;
+
 % number of CMIP6 models
 nmod=30;
 
-% annual max burned fraction
+% annual max burned fraction - here corresponding to the 100-yr return interval annual BA from the obs record
 capannual=0.075;
 
+% maximum about of post-fire tree regen failure
+alphaval=0.1;
+
+% period post-fire for tree regen failure
+nyeardrought=3;
+
+% read in estimated western US BA from Littell et al., 2009; calibrate for just forested land
 [wus]=csvread('westernusforest_extension.csv',1,0);
 wus=wus(:,2);
 [cmip6]=csvread('cmip6_model_cwd.csv',1,0);
@@ -28,7 +38,6 @@ cmip6=cmip6(101:251,2:31);
 % bias correct output
 
 [cmip6]=biascorrect(Z,cmip6,nmod);
-
 
 
 % loop over cmip models
@@ -47,7 +56,7 @@ for option=1:7
 [firehistory,rat1(:,option)]=spinupfirehistory(wus,option-1,totalarea,nmem);
 fire1950=firehistory(:,1950-1916);
 firehistory=firehistory(:,end-36);
-[r,p,FFA,ra,addr,raold,permold]=ba_feedbackmodel(BA,Z,totalarea,cmip6(:,model),option-1,capannual,nmem,firehistory,fire1950);
+[r,p,FFA,ra,addr,raold,permold]=ba_feedbackmodel(BA,Z,totalarea,cmip6(:,model),option-1,nmem,alphaval,nyeardrought,nsimulations,mcap,firehistory,fire1950);
 if model==1 
    rat1(69:105,option)=raold+permold;
 end
