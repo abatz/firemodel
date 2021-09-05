a71b5e17f3bd0151d03545320793449fa539522f
John <jabatzoglou@ucmerced.edu>
2021-09-05 16:34:38 -0700

    Update runfiremodel.m

--- runfiremodel.m
+++ runfiremodel.m
@@ -1,13 +1,13 @@
-function [BA,rat,permrat,firehistory]=runfiremodel(totalarea,firehistory,option,nmem,futureZ,Z5,addrat,a2,residual,capannual);
+function [BA,rat,permrat,firehistory]=runfiremodel(totalarea,firehistory,option,nmem,futureZ,Z5,addrat,a2,residual,mcap,alphaval,capannual);
 
-[rat,addrat,firehistory]=firehistorybufferadv(totalarea,firehistory,option,nmem,futureZ,Z5,addrat,.1);
+[rat,addrat,firehistory]=firehistorybufferadv(totalarea,firehistory,option,nmem,futureZ,Z5,addrat,alphaval,nyeardrought);
 
         
     BA=10.^(predict(a2,futureZ.*(1-rat)));
 
     % could also be some limit such that the burned area annually can not
     % exceed more than X% of existing land
-    BA=min(BA,(1-rat)*totalarea*capannual);
+    BA=min(BA,(1-rat)*totalarea*mcap);
     permrat=addrat;
     firehistory(end+1)=BA/totalarea;
     firehistory=firehistory(2:end);
